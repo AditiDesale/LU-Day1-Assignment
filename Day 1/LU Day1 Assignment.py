729875bf51b08d6e2f0fb14e39ca83f2e0b768fb
AditiDesale <71959787+AditiDesale@users.noreply.github.com>
2020-11-12 19:13:50 +0530

    Add files via upload
    LU Day1 Assignment submitted.

---
+++ Day 1/LU Day1 Assignment.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day 1 Assignment
+# 
+
+# # # correct word
+#  RAINBOW
+
+# In[1]:
+
+
+print("LETS UPGRADE")
+
+
+# In[5]:
+
+
+CP=int(input("cost price"))
+SP=int(input("selling price"))
+if CP>SP:
+    print("Loss")
+elif CP<SP:
+    print("Profit")
+else:
+    print("Neither")
+
+
+# In[6]:
+
+
+print(int(input())*80)
+
